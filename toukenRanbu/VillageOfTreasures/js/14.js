d592dd85fb22f16def717acd572039cc058ad74e
Suncat Huang <redsuncat@gmail.com>
2019-12-14 18:22:07 +0800

    Add `Village of Treasures 14` event.

--- toukenRanbu/VillageOfTreasures/js/14.js
+++ toukenRanbu/VillageOfTreasures/js/14.js
@@ -1,297 +1,407 @@
 var rewards = [
     {
-        "amount": 200000,
-        "reward": "北谷菜切",
+        "amount": 120000,
+        "reward": "桑名江",
         "highlight": true
     },
     {
-        "amount": 150000,
-        "reward": "手紙一式",
+        "amount": 116000,
+        "reward": "冷却材 × 4,000",
         "highlight": ""
     },
     {
-        "amount": 145000,
-        "reward": "旅道具",
+        "amount": 112000,
+        "reward": "玉鋼 × 4,000",
         "highlight": ""
     },
     {
-        "amount": 140000,
-        "reward": "旅装束",
+        "amount": 108000,
+        "reward": "木炭 × 4,000",
         "highlight": ""
     },
     {
-        "amount": 135000,
-        "reward": "御札˙富士",
-        "highlight": ""
-    },
-    {
-        "amount": 130000,
-        "reward": "玉鋼 × 3000",
-        "highlight": ""
-    },
-    {
-        "amount": 125000,
-        "reward": "砥石 × 3000",
-        "highlight": ""
-    },
-    {
-        "amount": 120000,
-        "reward": "冷却材 × 3000",
-        "highlight": ""
-    },
-    {
-        "amount": 115000,
-        "reward": "木炭 × 3000",
-        "highlight": ""
-    },
-    {
-        "amount": 110000,
-        "reward": "手伝い札 × 5",
-        "highlight": ""
-    },
-    {
-        "amount": 105000,
-        "reward": "依頼札 × 3",
+        "amount": 104000,
+        "reward": "砥石 × 4,000",
         "highlight": ""
     },
     {
         "amount": 100000,
-        "reward": "北谷菜切",
+        "reward": "松井江",
         "highlight": true
     },
     {
         "amount": 95000,
-        "reward": "蘇言機",
+        "reward": "蘇言機 × 2",
         "highlight": ""
     },
     {
         "amount": 90000,
-        "reward": "手紙一式",
+        "reward": "太鼓",
         "highlight": ""
     },
     {
         "amount": 85000,
-        "reward": "手伝い札 × 2",
+        "reward": "鈴",
         "highlight": ""
     },
     {
         "amount": 80000,
-        "reward": "巴形薙刀",
-        "highlight": true
-    },
-    {
-        "amount": 75000,
-        "reward": "玉鋼 × 1500",
-        "highlight": ""
-    },
-    {
-        "amount": 70000,
-        "reward": "旅道具",
-        "highlight": ""
-    },
-    {
-        "amount": 67500,
-        "reward": "砥石 × 1500",
-        "highlight": ""
-    },
-    {
-        "amount": 65000,
-        "reward": "冷却材 × 1500",
-        "highlight": ""
-    },
-    {
-        "amount": 62500,
-        "reward": "木炭 × 1500",
-        "highlight": ""
-    },
-    {
-        "amount": 60000,
-        "reward": "旅装束",
-        "highlight": ""
-    },
-    {
-        "amount": 57500,
-        "reward": "兵糧丸",
-        "highlight": ""
-    },
-    {
-        "amount": 55000,
-        "reward": "銃兵˙特上",
-        "highlight": ""
-    },
-    {
-        "amount": 52500,
-        "reward": "三倍魚籠",
-        "highlight": true
-    },
-    {
-        "amount": 50000,
-        "reward": "浦島虎徹",
-        "highlight": true
-    },
-    {
-        "amount": 47500,
-        "reward": "投石兵˙特上",
-        "highlight": ""
-    },
-    {
-        "amount": 45000,
         "reward": "手紙一式",
         "highlight": ""
     },
     {
-        "amount": 42500,
+        "amount": 75000,
+        "reward": "笛",
+        "highlight": ""
+    },
+    {
+        "amount": 70000,
+        "reward": "豊前江",
+        "highlight": true
+    },
+    {
+        "amount": 67500,
+        "reward": "琴",
+        "highlight": ""
+    },
+    {
+        "amount": 65000,
+        "reward": "三味線",
+        "highlight": ""
+    },
+    {
+        "amount": 62500,
+        "reward": "旅装束",
+        "highlight": ""
+    },
+    {
+        "amount": 60000,
+        "reward": "旅道具",
+        "highlight": ""
+    },
+    {
+        "amount": 57500,
+        "reward": "冷却材 × 3,000",
+        "highlight": ""
+    },
+    {
+        "amount": 55000,
+        "reward": "玉鋼 × 3,000",
+        "highlight": ""
+    },
+    {
+        "amount": 52500,
+        "reward": "木炭 × 3,000",
+        "highlight": ""
+    },
+    {
+        "amount": 51000,
+        "reward": "砥石 × 3,000",
+        "highlight": ""
+    },
+    {
+        "amount": 50000,
+        "reward": "篭手切江",
+        "highlight": true
+    },
+    {
+        "amount": 49000,
+        "reward": "槍兵˙特上",
+        "highlight": ""
+    },
+    {
+        "amount": 48000,
+        "reward": "盾兵˙特上",
+        "highlight": ""
+    },
+    {
+        "amount": 47000,
+        "reward": "重騎兵˙特上",
+        "highlight": ""
+    },
+    {
+        "amount": 46000,
+        "reward": "重歩兵˙特上",
+        "highlight": ""
+    },
+    {
+        "amount": 45000,
+        "reward": "銃兵˙特上",
+        "highlight": ""
+    },
+    {
+        "amount": 44000,
+        "reward": "投石兵˙特上",
+        "highlight": ""
+    },
+    {
+        "amount": 43000,
+        "reward": "軽騎兵˙特上",
+        "highlight": ""
+    },
+    {
+        "amount": 42000,
+        "reward": "軽歩兵˙特上",
+        "highlight": ""
+    },
+    {
+        "amount": 41000,
         "reward": "弓兵˙特上",
         "highlight": ""
     },
     {
         "amount": 40000,
-        "reward": "兵糧丸",
+        "reward": "小判箱˙大 × 2",
         "highlight": ""
     },
     {
-        "amount": 37500,
-        "reward": "旅道具",
+        "amount": 39000,
+        "reward": "手紙一式",
         "highlight": ""
     },
     {
-        "amount": 35000,
-        "reward": "御札˙松",
+        "amount": 38000,
+        "reward": "遠征呼び戻し鳩 × 2",
         "highlight": ""
     },
     {
-        "amount": 32500,
-        "reward": "盾兵˙特上",
-        "highlight": ""
-    },
-    {
-        "amount": 30000,
+        "amount": 37000,
         "reward": "旅装束",
         "highlight": ""
     },
     {
-        "amount": 27500,
-        "reward": "青毛",
-        "highlight": ""
-    },
-    {
-        "amount": 25000,
-        "reward": "兵糧丸",
-        "highlight": ""
-    },
-    {
-        "amount": 22500,
-        "reward": "槍兵˙特上",
-        "highlight": ""
-    },
-    {
-        "amount": 20000,
+        "amount": 36000,
         "reward": "御札˙竹",
         "highlight": ""
     },
     {
-        "amount": 18000,
-        "reward": "精鋭兵˙特上",
+        "amount": 35000,
+        "reward": "旅道具",
         "highlight": ""
     },
     {
-        "amount": 16000,
+        "amount": 34000,
+        "reward": "玉鋼 × 2,500",
+        "highlight": ""
+    },
+    {
+        "amount": 33000,
+        "reward": "木炭 × 2,500",
+        "highlight": ""
+    },
+    {
+        "amount": 32000,
+        "reward": "砥石 × 2,500",
+        "highlight": ""
+    },
+    {
+        "amount": 31000,
+        "reward": "冷却材 × 2,500",
+        "highlight": ""
+    },
+    {
+        "amount": 30000,
         "reward": "仙人団子",
         "highlight": ""
     },
     {
-        "amount": 14000,
-        "reward": "重騎兵˙特上",
-        "highlight": ""
-    },
-    {
-        "amount": 12000,
+        "amount": 29000,
         "reward": "御札˙梅",
         "highlight": ""
     },
     {
-        "amount": 10000,
-        "reward": "采配",
+        "amount": 28000,
+        "reward": "手伝い札 × 3",
         "highlight": ""
     },
     {
-        "amount": 9000,
-        "reward": "玉鋼 × 1000",
+        "amount": 27000,
+        "reward": "幕の内弁当",
         "highlight": ""
     },
     {
-        "amount": 8000,
-        "reward": "砥石 × 1000",
+        "amount": 26000,
+        "reward": "お守り",
         "highlight": ""
     },
     {
-        "amount": 7000,
-        "reward": "冷却材 × 1000",
+        "amount": 25000,
+        "reward": "鈴",
         "highlight": ""
     },
     {
-        "amount": 6000,
-        "reward": "木炭 × 1000",
-        "highlight": ""
-    },
-    {
-        "amount": 5000,
-        "reward": "三倍魚籠",
-        "highlight": true
-    },
-    {
-        "amount": 4000,
+        "amount": 24000,
         "reward": "遠征呼び戻し鳩",
         "highlight": ""
     },
     {
+        "amount": 23000,
+        "reward": "太鼓",
+        "highlight": ""
+    },
+    {
+        "amount": 22000,
+        "reward": "蘇言機 × 2",
+        "highlight": ""
+    },
+    {
+        "amount": 21000,
+        "reward": "幕の内弁当 × 2",
+        "highlight": ""
+    },
+    {
+        "amount": 20000,
+        "reward": "手伝い札 × 2",
+        "highlight": ""
+    },
+    {
+        "amount": 19000,
+        "reward": "木炭 × 1,000",
+        "highlight": ""
+    },
+    {
+        "amount": 18000,
+        "reward": "砥石 × 1,000",
+        "highlight": ""
+    },
+    {
+        "amount": 17000,
+        "reward": "冷却材 × 1,000",
+        "highlight": ""
+    },
+    {
+        "amount": 16000,
+        "reward": "三味線",
+        "highlight": ""
+    },
+    {
+        "amount": 15000,
+        "reward": "玉鋼 × 1,000",
+        "highlight": ""
+    },
+    {
+        "amount": 14000,
+        "reward": "琴",
+        "highlight": ""
+    },
+    {
+        "amount": 13000,
+        "reward": "精鋭兵˙特上",
+        "highlight": ""
+    },
+    {
+        "amount": 12000,
+        "reward": "笛",
+        "highlight": ""
+    },
+    {
+        "amount": 11000,
+        "reward": "仙人団子",
+        "highlight": ""
+    },
+    {
+        "amount": 10000,
+        "reward": "小判箱˙大 × 2",
+        "highlight": ""
+    },
+    {
+        "amount": 9000,
+        "reward": "蘇言機",
+        "highlight": ""
+    },
+    {
+        "amount": 8000,
+        "reward": "盾兵˙特上",
+        "highlight": ""
+    },
+    {
+        "amount": 7000,
+        "reward": "手伝の札 × 2",
+        "highlight": ""
+    },
+    {
+        "amount": 6000,
+        "reward": "小判箱˙大",
+        "highlight": ""
+    },
+    {
+        "amount": 5000,
+        "reward": "仙人団子",
+        "highlight": ""
+    },
+    {
+        "amount": 4500,
+        "reward": "銃兵˙特上",
+        "highlight": ""
+    },
+    {
+        "amount": 4000,
+        "reward": "槍兵˙特上",
+        "highlight": ""
+    },
+    {
+        "amount": 3500,
+        "reward": "投石兵˙特上",
+        "highlight": ""
+    },
+    {
         "amount": 3000,
-        "reward": "鹿毛",
+        "reward": "砥石 × 500",
+        "highlight": ""
+    },
+    {
+        "amount": 2500,
+        "reward": "冷却材 × 500",
         "highlight": ""
     },
     {
         "amount": 2000,
-        "reward": "重歩兵˙特上",
+        "reward": "玉鋼 × 500",
+        "highlight": ""
+    },
+    {
+        "amount": 1500,
+        "reward": "木炭 × 500",
         "highlight": ""
     },
     {
         "amount": 1000,
-        "reward": "手伝い札",
+        "reward": "重歩兵˙特上",
         "highlight": ""
     },
     {
         "amount": 750,
-        "reward": "軽騎兵˙特上",
+        "reward": "重騎兵˙特上",
         "highlight": ""
     },
     {
         "amount": 500,
-        "reward": "御札˙水砲",
+        "reward": "小判箱˙中 × 2",
         "highlight": ""
     },
     {
         "amount": 300,
+        "reward": "軽騎兵˙特上",
+        "highlight": ""
+    },
+    {
+        "amount": 200,
         "reward": "軽歩兵˙特上",
         "highlight": ""
     },
     {
         "amount": 100,
-        "reward": "三倍魚籠",
+        "reward": "手伝い札",
         "highlight": true
-    },
-    {
-        "amount": 30,
-        "reward": "兵糧丸",
-        "highlight": ""
     }
 ];
 var period = {
-    "startDate": '2019-07-30',
-    "endDate": '2019-08-27'
+    "startDate": '2019-12-10',
+    "endDate": '2019-12-24'
 };
 var currentAmountEl = $('#currentAmount');
 var rewardProgressEl = $('#rewardProgress');
-var highlightCookie = $.cookie('toukenRanbuRegimentBattleHLArray');
+var highlightCookie = $.cookie('toukenRanbuVOTHLArray');
 var highlightCookieArray = [];
 var today = new Date();
 var endDate = new Date(period['endDate']);
@@ -304,7 +414,7 @@ if (highlightCookie == undefined) {
         }
     });
     highlightCookieArray = highlightArray;
-    $.cookie('toukenRanbuRegimentBattleHLArray', highlightCookieArray, {expires: 30});
+    $.cookie('toukenRanbuVOTHLArray', highlightCookieArray, {expires: 30});
 }
 else {
     highlightCookieArray = highlightCookie.split(',');
@@ -316,7 +426,7 @@ else {
 $.each(period, function(i, v){
     $('#' + i).text(v);
 });
-currentAmountEl.val($.cookie('toukenRanbuRegimentBattleAmount'));
+currentAmountEl.val($.cookie('toukenRanbuVOTAmount'));
 createList();
 currentAmountEl.change(function() {
     var currentAmount = $(this).val();
@@ -329,7 +439,7 @@ currentAmountEl.change(function() {
         $('.text sub', entry).text(numbers['percentage'] + '%');
         $('.daily', entry).html(numbers['dailyText']);
     });
-    $.cookie('toukenRanbuRegimentBattleAmount', currentAmount, {expires: 30});
+    $.cookie('toukenRanbuVOTAmount', currentAmount, {expires: 30});
 });
 $('#addAmount').change(function(){
     var currentAmount = (currentAmountEl.val() == '' ? 0 : parseInt(currentAmountEl.val()))
@@ -338,7 +448,7 @@ $('#addAmount').change(function(){
     currentAmountEl.trigger('change');
     $(this).val('');
 });
-rewardProgressEl.after('<iframe class="likeCoin mt-2" data-v-36f4ac99="" src="https://button.like.co/in/embed/highlysensitivecat/button?referrer=https://redsuncat.github.io/toukenRanbu/RegimentBattle/2019-summer.html" frameborder="0" />');
+rewardProgressEl.after('<iframe class="likeCoin mt-2" data-v-36f4ac99="" src="https://button.like.co/in/embed/highlysensitivecat/button?referrer=https://redsuncat.github.io/toukenRanbu/VOT/2019-summer.html" frameborder="0" />');
 function createList () {
     var currentAmount = currentAmountEl.val();
     var diffDays = getDiffDays();
@@ -367,7 +477,7 @@ function createList () {
         else {
             highlightCookieArray.push(targetAmount);
         }
-        $.cookie('toukenRanbuRegimentBattleHLArray', highlightCookieArray, {expires: 30});
+        $.cookie('toukenRanbuVOTHLArray', highlightCookieArray, {expires: 30});
     });
 }
 function numberWithCommas(number) {
@@ -386,7 +496,7 @@ function getNumbers (targetAmount, currentAmount, diffDays) {
     var dailyAmount = Math.floor((targetAmount - currentAmount) / diffDays);
     var dailyText = '';
     if (today < endDate) {
-        dailyText = (dailyAmount > 0) ? ('每天至少需 ' + numberWithCommas(dailyAmount) + ' 貝') : '<span class="text-white">Get!</span>';
+        dailyText = (dailyAmount > 0) ? ('每天至少需 ' + numberWithCommas(dailyAmount) + ' 玉') : '<span class="get">Get!</span>';
     }
     var numbers = {
         'percentage': percentage,
